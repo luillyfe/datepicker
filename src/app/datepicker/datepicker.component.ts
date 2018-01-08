2bb524c0d3bedbc9be03aba6d4777c65feccf83a
luillyfe <luillyfe89@gmail.com>
2018-01-08 16:46:01 -0500

    - Changed default display for date-selector (Should be none) - Changed display to visivility property in order to work with transition - Created picker-month behaviour - Created picker-year behaviour

--- src/app/datepicker/datepicker.component.ts
+++ src/app/datepicker/datepicker.component.ts
@@ -7,22 +7,46 @@ import {DatepickerService} from './datepicker.service';
   styleUrls: ['./datepicker.component.css']
 })
 export class DatepickerComponent implements OnInit {
-    today: Date;
     month: Date[][];
+    year: Date[];
+    years: Date[];
     birthday: Date;
-    hiddenDateSelector = false;
+    hiddenDateSelector = true;
+    hiddenMonthSelector = true;
+    hiddenYearSelector = true;
+    hiddenDatePicker = false;
 
     constructor(private datepicker: DatepickerService) {}
 
     ngOnInit() {
         const date: Date = new Date();
 
-        this.today = new Date(Date.now());
+        this.birthday = new Date(Date.now());
         this.month = this.datepicker.getDays(new Date(date.getFullYear(), date.getMonth(), 1), date.getMonth());
+        this.year = this.datepicker.getMonths(date.getFullYear());
+        this.years = this.datepicker.getYears();
     }
 
-    showOff() {
+    datePicker(day: Date) {
+        this.birthday = day;
         this.hiddenDateSelector = true;
+    }
+
+    monthPicker(month: Date) {
+        this.hiddenMonthSelector = true;
+        this.hiddenYearSelector = true;
+
+        this.birthday = month;
+        this.month = this.datepicker.getDays(new Date(this.birthday.getFullYear(), this.birthday.getMonth(), 1), this.birthday.getMonth());
+    }
+
+    yearPicker(year: Date) {
+        this.hiddenMonthSelector = true;
+        this.hiddenYearSelector = true;
+
+        this.birthday = year;
+        this.year = this.datepicker.getMonths(year.getFullYear());
+        this.month = this.datepicker.getDays(new Date(this.birthday.getFullYear(), 0, 1), 0);
     }
 
 }
--- src/app/datepicker/datepicker.service.ts
+++ src/app/datepicker/datepicker.service.ts
@@ -3,17 +3,33 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class DatepickerService {
 
-  getDays(date: Date, month: number): Date[][] {
-      const days: Date[][] = [[], [], [], [], []];
-      let week = 0;
-      while (date.getMonth() === month) {
-          for (let i = 0; i < 7; i++) {
-              days[week].push(new Date(date));
-              date.setDate(date.getDate() + 1);
-          }
-          week++;
-      }
-      return days;
-  }
+    getDays(date: Date, month: number): Date[][] {
+        const days: Date[][] = [[], [], [], [], []];
+        let week = 0;
+        while (date.getMonth() === month) {
+            for (let i = 0; i < 7; i++) {
+                days[week].push(new Date(date));
+                date.setDate(date.getDate() + 1);
+            }
+            week++;
+        }
+        return days;
+    }
+
+    getMonths(year: number) {
+        const months = [];
+        for (let month = 0; month < 12; month++) {
+            months.push(new Date(year, month, 1));
+        }
+        return months;
+    }
+
+    getYears() {
+        const years = [], currentYear = new Date().getFullYear();
+        for (let year = 1970; year <= currentYear; year++) {
+            years.push(new Date(year, 0, 1));
+        }
+        return years;
+    }
 
 }
